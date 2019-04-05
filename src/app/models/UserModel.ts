caf2ce8b297359d11e8dc3c0e2eb7644dfe898cc
Wiktor Krzyżanowski <wiktor.krzyanowski1@gmail.com>
2019-04-05 11:57:41 +0200

    Login working with token in cookies

---
+++ src/app/models/UserModel.ts
@@ -0,0 +1,9 @@
+export class UserModel {
+  userId?: string;
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  active?: boolean;
+  department?: string;
+  roles?: Array<string>;
+}
