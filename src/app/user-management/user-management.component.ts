fe0cbe10acc4cd243ba4dc587186e8bbe9e26b04
Wiktor Krzyżanowski <wiktor.krzyanowski1@gmail.com>
2019-04-11 16:13:33 +0200

    Edition in work but almost done

--- src/app/user-management/user-management.component.ts
+++ src/app/user-management/user-management.component.ts
@@ -7,6 +7,7 @@ import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrie
 import { Event } from '@angular/router';
 import { timer } from 'rxjs';
 import { UserRoles } from '../models/Roles.enum';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-user-management',
@@ -16,6 +17,12 @@ import { UserRoles } from '../models/Roles.enum';
 export class UserManagementComponent implements OnInit {
   loggedUserModel: UserModel;
 
+  sentDataButtonNotActive: boolean;
+  submitDataButtonNotActive: boolean;
+
+  editedElementList: HTMLCollection = document.getElementsByClassName(
+    'editedField'
+  );
   usersList: Array<UserModel>;
   statuses: Array<boolean> = [true, false];
 
@@ -25,30 +32,76 @@ export class UserManagementComponent implements OnInit {
     private departmentService: DepartmentService
   ) {
     this.loggedUserModel = this.loginService.loggedUser;
+    this.sentDataButtonNotActive = true;
   }
 
   ngOnInit() {
-    timer(500).subscribe(action => {
+    timer(700).subscribe(action => {
       this.callApi();
     });
   }
 
-  changeEditedFieldBackground(event, userId: string, isRole: boolean) {
-    let currentTarget: string = event.currentTarget.id;
+  changeEditedFieldBackground(event, userId: string, role?: string) {
+    const currentTarget: string = event.currentTarget.id;
     document.getElementById(currentTarget).classList.toggle('editedField');
+    document.getElementById(currentTarget).classList.toggle('normalField');
     console.log('User ID:' + userId + ' changed field: ' + currentTarget);
-
     this.usersList.forEach(user => {
       if (user.userId === userId) {
-        if (currentTarget.includes('stat')) {
+        if (currentTarget.includes('status')) {
           user.active = !user.active;
+        }
+        if (role !== undefined) {
+          const roleIndex = user.roles.indexOf(role);
+          if (roleIndex !== -1) {
+            user.roles.splice(roleIndex, 1);
+          } else {
+            user.roles.push(role.toUpperCase());
+          }
         }
       }
     });
+    this.editedElementList = document.getElementsByClassName('editedField');
   }
 
   showAll() {
+    const htmlCollection = this.getCopyOfHtmlCollection();
+    if (htmlCollection.length !== 0) {
+      for (const targetElementID of htmlCollection) {
+        const inputElement = document.getElementById(targetElementID)
+          .firstChild as HTMLInputElement;
+        inputElement.disabled = true;
+      }
+      this.submitDataButtonNotActive = true;
+      this.sentDataButtonNotActive = false;
+    }
+  }
+
+  sentData() {
+    if (confirm('ARE YOU SURE TO MAKE CHANGES?')) {
+      const htmlCollection = this.getCopyOfHtmlCollection();
+      for (const targetElementID of htmlCollection) {
+        const inputField = document.getElementById(targetElementID);
+        inputField.classList.remove('editedField');
+        inputField.classList.add('submittedField');
+      }
+    } else {
+      console.log('NO');
+    }
+    this.sentDataButtonNotActive = true;
+  }
+
+  getCopyOfHtmlCollection(): Array<string> {
+    let copiedList: Array<string> = new Array();
+    for (var i = 0; i < this.editedElementList.length; i++) {
+      copiedList.push(this.editedElementList[i].id);
+    }
+    return copiedList;
+  }
+
+  getDebugList(): void {
     console.log(this.usersList);
+    console.log(this.editedElementList);
   }
 
   callApi() {
